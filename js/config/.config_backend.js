57adac86ffc1645600195bd62b4d0518f8d74d7e
Confoederatio <htmlprogrammingbn@gmail.com>
2025-04-27 23:37:16 +0100

    deleted website files

--- js/config/.config_backend.js
+++
@@ -1,8 +0,0 @@
-window.config = {};
-config.homepage = {};
-  //Defines for selectors and other constants
-  config.homepage.defines = {};
-
-  //Config parsers
-  config.homepage.banner = {};
-  config.homepage.gallery = {};
